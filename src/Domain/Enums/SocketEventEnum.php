6d50fb9227cb61ba0f8c04db25055645473c5835
Vitaliy <theyamshikov@yandex.ru>
2020-09-17 21:45:31 +0600

    upd

---
+++ src/Domain/Enums/SocketEventEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ZnLib\Socket\Domain\Enums;
+
+class SocketEventEnum {
+
+    const CONNECT = 'connect';
+    const DISCONNECT = 'disconnect';
+    const MESSAGE = 'message';
+
+}
---
+++ src/Domain/Enums/SocketEventStatusEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ZnLib\Socket\Domain\Enums;
+
+class SocketEventStatusEnum {
+
+    const OK = 200;
+    const ERROR = 500;
+
+}
